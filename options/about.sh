6d87da194be0090b6c64c370d88aa1e91f5c55cc
nexus-bot-dev <ayfa7756@gmail.com>
2025-11-04 17:13:39 +0700

    Add files via upload

---
+++ options/about.sh
@@ -0,0 +1,18 @@
+echo -e "┌─────────────────────────────────────────────────┐" | lolcat
+echo -e "│               AUTOSCRIPT PREMIUM                │" | lolcat
+echo -e "│             NEXUS TUNNELLING              │" | lolcat
+echo -e "└─────────────────────────────────────────────────┘" | lolcat
+
+echo -e "> Support OS 
+  For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+  For Debian 9 & Debian 10 & Debian 11 " | lolcat
+echo ""
+echo -e "  Contact Owner AutoScript
+> WA: +6282380835972
+> TELEGRAM: t.me/D_swara" | lolcat
+echo ""
+echo -e "Thanks for your Order VPN EXPRESS" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
